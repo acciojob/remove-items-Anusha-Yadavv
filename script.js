fad2951d27f4a3d8adc2aecb2545707daf35021c
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-21 13:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function removeItems() {
+	let selectElement=document.getElementById("colorSelect");
+	let sectedItemIndex=selectElement.selectedIndex;
+	selectElement.remove(sectedItemIndex);
+	
+	
+}
